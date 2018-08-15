0b850bba03f665236af6662b3749f42317d5c1a3
d4 <d4@sunshine>
2018-08-15 03:24:38 +0200

    examdates models modified

--- examdates/models.py
+++ examdates/models.py
@@ -5,13 +5,14 @@ class Country(models.Model):
     name = models.CharField(max_length=100)
     cid = models.IntegerField()  # country id at council site #
     currency = models.CharField(max_length=10)
-    last_seen = models.DateTimeField()
+    last_seen = models.DateTimeField(null=True)
 
 class City(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    venue = models.CharField(null=True, max_length=300)
     cid = models.CharField(max_length=100)  # city name at council site #
-    last_seen = models.DateTimeField()
+    last_seen = models.DateTimeField(null=True)
 
 class Date(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
@@ -19,7 +20,7 @@ class Date(models.Model):
     date = models.DateField()
     module = models.CharField(max_length=1)  # [A]cademic, [G]eneral Training #
     fee = models.DecimalField(max_digits=9, decimal_places=2)
-    last_seen = models.DateTimeField()
+    last_seen = models.DateTimeField(null=True)
     available = models.IntegerField()
 
 
